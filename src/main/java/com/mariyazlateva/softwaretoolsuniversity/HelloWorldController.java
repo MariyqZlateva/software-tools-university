07d78cbca7114b9e940a99c7903260f866cc65e9
MariyaZlateva <mimitoooo@abv.bg>
2021-05-16 13:59:32 +0300

    Changed message from controller.

--- src/main/java/com/mariyazlateva/softwaretoolsuniversity/HelloWorldController.java
+++ src/main/java/com/mariyazlateva/softwaretoolsuniversity/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
     @GetMapping
     public String getHelloWorldMessage() {
-        return "Hello World Updated!";
+        return "Hello World Updated Again!";
     }
 
 }
