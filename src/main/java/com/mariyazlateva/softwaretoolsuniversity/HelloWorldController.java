762541f2544b4d70a28a79d0be0c2de85d37cc50
MariyaZlateva <mimitoooo@abv.bg>
2021-05-16 12:45:38 +0300

    Initial commit

---
+++ src/main/java/com/mariyazlateva/softwaretoolsuniversity/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.mariyazlateva.softwaretoolsuniversity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping
+    public String getHelloWorldMessage() {
+        return "Hello World!";
+    }
+
+}
---
+++ src/test/java/com/mariyazlateva/softwaretoolsuniversity/SoftwareToolsUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariyazlateva.softwaretoolsuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareToolsUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
